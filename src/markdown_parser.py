5dcb4c3e76dead3a7edf371300273182be88c90a
dimitrisLivanas <livanas.dimitris@gmail.com>
2024-09-24 11:50:06 +0300

    Split images and links

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -1,4 +1,5 @@
 from textnode import TextNode
+from markdown_extractor import extract_markdown_images, extract_markdown_links
 
 text_type_text = "text"
 text_type_bold = "bold"
@@ -24,5 +25,53 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     new_nodes.append(TextNode(part, text_type_text))
             else:
                 new_nodes.append(TextNode(part, text_type))
+
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+
+        images = extract_markdown_images(node.text)
+        if not images:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        for alt, url in images:
+            before, text = text.split(f"![{alt}]({url})", 1)
+            if before:
+                new_nodes.append(TextNode(before, text_type_text))
+            new_nodes.append(TextNode(alt, text_type_image, url))
+
+        if text:
+            new_nodes.append(TextNode(text, text_type_text))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+
+        links = extract_markdown_links(node.text)
+        if not links:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        for anchor, url in links:
+            before, text = text.split(f"[{anchor}]({url})", 1)
+            if before:
+                new_nodes.append(TextNode(before, text_type_text))
+            new_nodes.append(TextNode(anchor, text_type_link, url))
+
+        if text:
+            new_nodes.append(TextNode(text, text_type_text))
 
     return new_nodes
--- src/test_markdown_parser.py
+++ src/test_markdown_parser.py
@@ -1,6 +1,14 @@
 import unittest
 from textnode import TextNode
-from markdown_parser import split_nodes_delimiter, text_type_text, text_type_bold, text_type_italic, text_type_code
+from markdown_parser import (split_nodes_delimiter,
+                             text_type_text,
+                             text_type_bold,
+                             text_type_italic,
+                             text_type_code,
+                             text_type_image,
+                             text_type_link,
+                             split_nodes_image,
+                             split_nodes_link)
 
 class TestMarkdownParser(unittest.TestCase):
     def test_split_text_with_code(self):
@@ -37,6 +45,40 @@ class TestMarkdownParser(unittest.TestCase):
         node = TextNode("This is `unmatched code block", text_type_text)
         with self.assertRaises(ValueError):
             split_nodes_delimiter([node], "`", text_type_code)
+
+    def test_split_nodes_image(self):
+        node = TextNode("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)", text_type_text)
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("rick roll", text_type_image, "https://i.imgur.com/aKaOqIh.gif"),
+            TextNode(" and ", text_type_text),
+            TextNode("obi wan", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_nodes_link(self):
+        node = TextNode("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)", text_type_text)
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with a link ", text_type_text),
+            TextNode("to boot dev", text_type_link, "https://www.boot.dev"),
+            TextNode(" and ", text_type_text),
+            TextNode("to youtube", text_type_link, "https://www.youtube.com/@bootdotdev"),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_nodes_image_no_images(self):
+        node = TextNode("This is text with no images.", text_type_text)
+        new_nodes = split_nodes_image([node])
+        expected = [node]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_nodes_link_no_links(self):
+        node = TextNode("This is text with no links.", text_type_text)
+        new_nodes = split_nodes_link([node])
+        expected = [node]
+        self.assertEqual(new_nodes, expected)
 
 if __name__ == "__main__":
     unittest.main()
